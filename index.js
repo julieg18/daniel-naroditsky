d3f838263c24e570c878b03ba74d5120f1535a34
julieg18 <juliannagal18@gmail.com>
2020-12-18 09:02:43 -0600

    Updates

--- index.js
+++ index.js
@@ -19,7 +19,7 @@
 /*! runtime requirements: __webpack_require__, __webpack_require__.r, __webpack_exports__, __webpack_require__.* */
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _pages_index_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../pages/index.css */ \"./src/pages/index.css\");\n\n\n//# sourceURL=webpack://daniel-naroditsky/./src/scripts/index.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _pages_index_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../pages/index.css */ \"./src/pages/index.css\");\n\nconsole.log('this is the index page');\n\n//# sourceURL=webpack://daniel-naroditsky/./src/scripts/index.js?");
 
 /***/ }),
 
