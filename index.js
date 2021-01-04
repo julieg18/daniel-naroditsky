40a34ed8ca974e21b1f3b7576f92ca3650bea951
julieg18 <juliannagal18@gmail.com>
2021-01-04 06:52:16 -0600

    Updates

--- index.js
+++ index.js
@@ -10,6 +10,36 @@
 /******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
 
+/***/ "./src/scripts/blocks/nav.js":
+/*!***********************************!*\
+  !*** ./src/scripts/blocks/nav.js ***!
+  \***********************************/
+/*! namespace exports */
+/*! exports [not provided] [no usage info] */
+/*! runtime requirements: __webpack_require__, __webpack_require__.r, __webpack_exports__, __webpack_require__.* */
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _constants_index__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../constants/index */ \"./src/scripts/constants/index.js\");\n\n\nfunction handlePageClick(e) {\n  var el = e.target;\n  var isElMenuBtn = el.classList.contains('la-bars');\n  var isElNavItems = el === _constants_index__WEBPACK_IMPORTED_MODULE_0__.navItems || _constants_index__WEBPACK_IMPORTED_MODULE_0__.navItems.contains(el);\n  var isElNavLink = el.classList.contains('nav__link');\n\n  if (!isElMenuBtn && !isElNavItems || isElNavLink) {\n    // eslint-disable-next-line no-use-before-define\n    closeNavMenu();\n  }\n}\n\nfunction openNavMenu() {\n  _constants_index__WEBPACK_IMPORTED_MODULE_0__.navItems.classList.add('nav__items_show');\n  _constants_index__WEBPACK_IMPORTED_MODULE_0__.pageBackground.addEventListener('click', handlePageClick);\n}\n\nfunction closeNavMenu() {\n  _constants_index__WEBPACK_IMPORTED_MODULE_0__.navItems.classList.remove('nav__items_show');\n  _constants_index__WEBPACK_IMPORTED_MODULE_0__.pageBackground.removeEventListener('click', handlePageClick);\n}\n\n_constants_index__WEBPACK_IMPORTED_MODULE_0__.navMenuButton.addEventListener('click', openNavMenu);\n_constants_index__WEBPACK_IMPORTED_MODULE_0__.navExitButton.addEventListener('click', closeNavMenu);\n\n//# sourceURL=webpack://daniel-naroditsky/./src/scripts/blocks/nav.js?");
+
+/***/ }),
+
+/***/ "./src/scripts/constants/index.js":
+/*!****************************************!*\
+  !*** ./src/scripts/constants/index.js ***!
+  \****************************************/
+/*! namespace exports */
+/*! export navExitButton [provided] [no usage info] [missing usage info prevents renaming] */
+/*! export navItems [provided] [no usage info] [missing usage info prevents renaming] */
+/*! export navMenuButton [provided] [no usage info] [missing usage info prevents renaming] */
+/*! export pageBackground [provided] [no usage info] [missing usage info prevents renaming] */
+/*! other exports [not provided] [no usage info] */
+/*! runtime requirements: __webpack_require__.r, __webpack_exports__, __webpack_require__.d, __webpack_require__.* */
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"pageBackground\": () => /* binding */ pageBackground,\n/* harmony export */   \"navMenuButton\": () => /* binding */ navMenuButton,\n/* harmony export */   \"navExitButton\": () => /* binding */ navExitButton,\n/* harmony export */   \"navItems\": () => /* binding */ navItems\n/* harmony export */ });\nvar pageBackground = document.querySelector('.page__background');\nvar navMenuButton = document.querySelector('.nav__btn_icon_menu');\nvar navExitButton = document.querySelector('.nav__btn_icon_exit');\nvar navItems = document.querySelector('.nav__items');\n\n\n//# sourceURL=webpack://daniel-naroditsky/./src/scripts/constants/index.js?");
+
+/***/ }),
+
 /***/ "./src/scripts/index.js":
 /*!******************************!*\
   !*** ./src/scripts/index.js ***!
@@ -19,7 +49,7 @@
 /*! runtime requirements: __webpack_require__, __webpack_require__.r, __webpack_exports__, __webpack_require__.* */
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _pages_index_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../pages/index.css */ \"./src/pages/index.css\");\n\nvar menuButton = document.querySelector('.nav__btn_icon_menu');\nvar exitButton = document.querySelector('.nav__btn_icon_exit');\nvar navItems = document.querySelector('.nav__items');\nmenuButton.addEventListener('click', function (e) {\n  navItems.classList.toggle('nav__items_show');\n});\nexitButton.addEventListener('click', function () {\n  navItems.classList.toggle('nav__items_show');\n});\n\n//# sourceURL=webpack://daniel-naroditsky/./src/scripts/index.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _blocks_nav__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./blocks/nav */ \"./src/scripts/blocks/nav.js\");\n/* harmony import */ var _pages_index_css__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../pages/index.css */ \"./src/pages/index.css\");\n\n\n\n//# sourceURL=webpack://daniel-naroditsky/./src/scripts/index.js?");
 
 /***/ }),
 
@@ -62,6 +92,23 @@ eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extr
 /******/ 	}
 /******/ 	
 /************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => Object.prototype.hasOwnProperty.call(obj, prop)
+/******/ 	})();
+/******/ 	
 /******/ 	/* webpack/runtime/make namespace object */
 /******/ 	(() => {
 /******/ 		// define __esModule on exports
