cca2264832e05d9a1992030000dc436945a4abb4
julieg18 <juliannagal18@gmail.com>
2021-01-05 07:59:00 -0600

    fix: fix missing env vars on published site

--- webpack.config.js
+++ webpack.config.js
@@ -51,7 +51,7 @@ module.exports = {
     ],
   },
   plugins: [
-    new Dotenv(),
+    new Dotenv({ systemvars: true }),
     new HtmlWebpackPlugin({
       template: './src/index.html',
       chunks: ['index'],
