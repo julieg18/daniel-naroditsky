a8226b1eb501815e9230411db5a67575d3419f57
julieg18 <juliannagal18@gmail.com>
2021-01-05 05:37:31 -0600

    Updates

--- merch.js
+++ merch.js
@@ -10,17 +10,35 @@
 /******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
 
-/***/ "./src/scripts/components/CheckboxInputGroup.js":
-/*!******************************************************!*\
-  !*** ./src/scripts/components/CheckboxInputGroup.js ***!
-  \******************************************************/
+/***/ "./src/scripts/blocks/nav.js":
+/*!***********************************!*\
+  !*** ./src/scripts/blocks/nav.js ***!
+  \***********************************/
 /*! namespace exports */
-/*! export default [provided] [no usage info] [missing usage info prevents renaming] */
-/*! other exports [not provided] [no usage info] */
-/*! runtime requirements: __webpack_exports__, __webpack_require__.r, __webpack_require__.d, __webpack_require__.* */
+/*! exports [not provided] [no usage info] */
+/*! runtime requirements: __webpack_require__, __webpack_require__.r, __webpack_exports__, __webpack_require__.* */
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => __WEBPACK_DEFAULT_EXPORT__\n/* harmony export */ });\nfunction _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError(\"Cannot call a class as a function\"); } }\n\nfunction _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if (\"value\" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }\n\nfunction _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }\n\nvar CheckboxInputGroup = /*#__PURE__*/function () {\n  function CheckboxInputGroup(_ref) {\n    var labelSelector = _ref.labelSelector,\n        callback = _ref.callback;\n\n    _classCallCheck(this, CheckboxInputGroup);\n\n    this._labels = Array.from(document.querySelectorAll(labelSelector));\n    this._inputs = this._labels.map(function (label) {\n      return label.querySelector('input');\n    });\n    this._callback = callback;\n  }\n\n  _createClass(CheckboxInputGroup, [{\n    key: \"setValues\",\n    value: function setValues(values) {\n      this._inputs.forEach(function (input) {\n        input.checked = values.includes(input.value);\n      });\n    }\n  }, {\n    key: \"getValues\",\n    value: function getValues() {\n      var values = [];\n\n      this._inputs.forEach(function (input) {\n        if (input.checked) {\n          values.push(input.value);\n        }\n      });\n\n      return values;\n    }\n  }, {\n    key: \"setEventListeners\",\n    value: function setEventListeners() {\n      var _this = this;\n\n      this._labels.forEach(function (label) {\n        label.querySelector('click', function () {\n          console.log('LABEL HAS BEEN CLICKED');\n        });\n      });\n\n      this._inputs.forEach(function (input) {\n        input.addEventListener('click', function () {\n          _this._callback(_this.getValues());\n        });\n      });\n    }\n  }]);\n\n  return CheckboxInputGroup;\n}();\n\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (CheckboxInputGroup);\n\n//# sourceURL=webpack://daniel-naroditsky/./src/scripts/components/CheckboxInputGroup.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _constants_index__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../constants/index */ \"./src/scripts/constants/index.js\");\n\n\nfunction handlePageClick(e) {\n  var el = e.target;\n  var isElMenuBtn = el.classList.contains('la-bars');\n  var isElNavItems = el === _constants_index__WEBPACK_IMPORTED_MODULE_0__.navItems || _constants_index__WEBPACK_IMPORTED_MODULE_0__.navItems.contains(el);\n  var isElNavLink = el.classList.contains('nav__link');\n\n  if (!isElMenuBtn && !isElNavItems || isElNavLink) {\n    // eslint-disable-next-line no-use-before-define\n    closeNavMenu();\n  }\n}\n\nfunction openNavMenu() {\n  _constants_index__WEBPACK_IMPORTED_MODULE_0__.navItems.classList.add('nav__items_show');\n  _constants_index__WEBPACK_IMPORTED_MODULE_0__.pageBackground.addEventListener('click', handlePageClick);\n}\n\nfunction closeNavMenu() {\n  _constants_index__WEBPACK_IMPORTED_MODULE_0__.navItems.classList.remove('nav__items_show');\n  _constants_index__WEBPACK_IMPORTED_MODULE_0__.pageBackground.removeEventListener('click', handlePageClick);\n}\n\n_constants_index__WEBPACK_IMPORTED_MODULE_0__.navMenuButton.addEventListener('click', openNavMenu);\n_constants_index__WEBPACK_IMPORTED_MODULE_0__.navExitButton.addEventListener('click', closeNavMenu);\n\n//# sourceURL=webpack://daniel-naroditsky/./src/scripts/blocks/nav.js?");
+
+/***/ }),
+
+/***/ "./src/scripts/constants/index.js":
+/*!****************************************!*\
+  !*** ./src/scripts/constants/index.js ***!
+  \****************************************/
+/*! namespace exports */
+/*! export navExitButton [provided] [no usage info] [missing usage info prevents renaming] */
+/*! export navItems [provided] [no usage info] [missing usage info prevents renaming] */
+/*! export navMenuButton [provided] [no usage info] [missing usage info prevents renaming] */
+/*! export pageBackground [provided] [no usage info] [missing usage info prevents renaming] */
+/*! export videosNewVideos [provided] [no usage info] [missing usage info prevents renaming] */
+/*! export videosPopularVideos [provided] [no usage info] [missing usage info prevents renaming] */
+/*! other exports [not provided] [no usage info] */
+/*! runtime requirements: __webpack_require__.r, __webpack_exports__, __webpack_require__.d, __webpack_require__.* */
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"pageBackground\": () => /* binding */ pageBackground,\n/* harmony export */   \"navMenuButton\": () => /* binding */ navMenuButton,\n/* harmony export */   \"navExitButton\": () => /* binding */ navExitButton,\n/* harmony export */   \"navItems\": () => /* binding */ navItems,\n/* harmony export */   \"videosNewVideos\": () => /* binding */ videosNewVideos,\n/* harmony export */   \"videosPopularVideos\": () => /* binding */ videosPopularVideos\n/* harmony export */ });\nvar pageBackground = document.querySelector('.page__background');\nvar navMenuButton = document.querySelector('.nav__btn_icon_menu');\nvar navExitButton = document.querySelector('.nav__btn_icon_exit');\nvar navItems = document.querySelector('.nav__items');\nvar videosNewVideos = Array.from(document.querySelectorAll('.videos__list_group_new .video'));\nvar videosPopularVideos = Array.from(document.querySelectorAll('.videos__list_group_popular .video'));\n\n\n//# sourceURL=webpack://daniel-naroditsky/./src/scripts/constants/index.js?");
 
 /***/ }),
 
@@ -33,7 +51,7 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpac
 /*! runtime requirements: __webpack_require__, __webpack_require__.r, __webpack_exports__, __webpack_require__.* */
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _components_CheckboxInputGroup__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./components/CheckboxInputGroup */ \"./src/scripts/components/CheckboxInputGroup.js\");\n/* harmony import */ var _pages_merch_css__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../pages/merch.css */ \"./src/pages/merch.css\");\n\n\nvar menuButton = document.querySelector('.nav__btn_icon_menu');\nvar exitButton = document.querySelector('.nav__btn_icon_exit');\nvar navItems = document.querySelector('.nav__items');\nmenuButton.addEventListener('click', function (e) {\n  navItems.classList.toggle('nav__items_show');\n});\nexitButton.addEventListener('click', function () {\n  navItems.classList.toggle('nav__items_show');\n});\nvar filterMerchCategories = new _components_CheckboxInputGroup__WEBPACK_IMPORTED_MODULE_0__.default({\n  labelSelector: '.filter-merch__category',\n  callback: function callback() {\n    return console.log('callback has been called');\n  }\n});\nfilterMerchCategories.setEventListeners();\ndocument.querySelector('.filter-merch__category-btn').addEventListener('click', function () {\n  return console.log('button has been clicked!');\n});\n\n//# sourceURL=webpack://daniel-naroditsky/./src/scripts/merch.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _blocks_nav__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./blocks/nav */ \"./src/scripts/blocks/nav.js\");\n/* harmony import */ var _pages_merch_css__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../pages/merch.css */ \"./src/pages/merch.css\");\n\n\n\n//# sourceURL=webpack://daniel-naroditsky/./src/scripts/merch.js?");
 
 /***/ }),
 
