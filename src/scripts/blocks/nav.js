154bb1872bb66996f2124448afa456c112122251
julieg18 <juliannagal18@gmail.com>
2021-01-06 05:37:53 -0600

    fix: fix tabbable hidden nav items

--- src/scripts/blocks/nav.js
+++ src/scripts/blocks/nav.js
@@ -3,11 +3,23 @@ import {
   navMenuButton,
   navExitButton,
   navItems,
+  navLinks,
 } from '../constants/index';
+
+function changeNavItemsTabIndex(index) {
+  if (navExitButton.tabIndex.toString() !== index.toString()) {
+    navLinks.forEach((link) => {
+      link.tabIndex = index;
+    });
+    navExitButton.tabIndex = index;
+  }
+}
 
 function handlePageClick(e) {
   const el = e.target;
-  const isElMenuBtn = el.classList.contains('la-bars');
+  const isElMenuBtn =
+    el.classList.contains('la-bars') ||
+    el.classList.contains('nav__btn_icon_menu');
   const isElNavItems = el === navItems || navItems.contains(el);
   const isElNavLink = el.classList.contains('nav__link');
 
@@ -20,12 +32,22 @@ function handlePageClick(e) {
 function openNavMenu() {
   navItems.classList.add('nav__items_show');
   pageBackground.addEventListener('click', handlePageClick);
+  changeNavItemsTabIndex(0);
 }
 
 function closeNavMenu() {
   navItems.classList.remove('nav__items_show');
   pageBackground.removeEventListener('click', handlePageClick);
+  changeNavItemsTabIndex(-1);
 }
 
+function handleScreenChange() {
+  if (!navItems.classList.contains('nav__items_show')) {
+    changeNavItemsTabIndex(document.body.offsetWidth < 1280 ? -1 : 0);
+  }
+}
+
+handleScreenChange();
 navMenuButton.addEventListener('click', openNavMenu);
 navExitButton.addEventListener('click', closeNavMenu);
+window.addEventListener('resize', handleScreenChange);
