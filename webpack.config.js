916f38606d8143e3423d5e8d8ba7efa162480912
julieg18 <juliannagal18@gmail.com>
2021-01-08 07:27:07 -0600

    feat: add favicon

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
 
 module.exports = {
@@ -66,6 +67,29 @@ module.exports = {
       filename: '404.html',
       template: './src/404.html',
       chunks: ['404'],
+    }),
+    new FaviconsWebpackPlugin({
+      logo: './src/images/logo.png',
+      favicons: {
+        icons: {
+          appleIcon: [
+            'apple-touch-icon-114x114.png',
+            'apple-touch-icon-120x120.png',
+            'apple-touch-icon-144x144.png',
+            'apple-touch-icon-152x152.png',
+            'apple-touch-icon-167x167.png',
+            'apple-touch-icon-180x180.png',
+            'apple-touch-icon-57x57.png',
+            'apple-touch-icon-60x60.png',
+            'apple-touch-icon-72x72.png',
+            'apple-touch-icon-76x76.png',
+            'apple-touch-icon-precomposed.png',
+            'apple-touch-icon.png',
+          ],
+          appleStartup: false,
+          firefox: ['firefox_app_128x128.png', 'firefox_app_60x60.png'],
+        },
+      },
     }),
     new MiniCssExtractPlugin(),
   ],
