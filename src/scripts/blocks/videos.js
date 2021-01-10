1ab0ebea3927fa70c308080558dcb7fe2a7ac2b7
julieg18 <juliannagal18@gmail.com>
2021-01-10 17:27:01 -0600

    feat: update how youtube videos are loaded for SEO

--- src/scripts/blocks/videos.js
+++ src/scripts/blocks/videos.js
@@ -1,5 +1,9 @@
 import { videosNewVideos, videosPopularVideos } from '../constants/index';
 
+const getVideoElSrc = (videoId) => `https://www.youtube.com/embed/${videoId}`;
+const getVideoElSrcDoc = ({ videoId, title }) =>
+  `<style>*{padding:0;margin:0;overflow:hidden}html,body{height:100%}img,span{position:absolute;width:100%;top:0;bottom:0;margin:auto}span{height:1.5em;text-align:center;font:48px/1.5 sans-serif;color:white;text-shadow:0 0 0.5em black}</style><a href=https://www.youtube.com/embed/${videoId}?autoplay=1><img src=https://img.youtube.com/vi/${videoId}/hqdefault.jpg alt='${title}'><span>▶</span></a>`;
+
 function sendGetRequest({ url }) {
   return fetch(url).then((res) => {
     if (res.ok) {
@@ -12,16 +16,22 @@ function sendGetRequest({ url }) {
 function getYoutubeChannelsNewestVideos() {
   const url = `https://www.googleapis.com/youtube/v3/search?key=${process.env.YOUTUBE_API_KEY}&channelId=${process.env.YOUTUBE_CHANNEL_ID}&part=snippet,id&maxResults=5&order=date`;
   return sendGetRequest({ url }).then(({ items }) => {
-    const videoIds = items.map(({ id: { videoId } }) => videoId);
-    return videoIds;
+    const videos = items.map(({ id: { videoId }, snippet: { title } }) => ({
+      title,
+      videoId,
+    }));
+    return videos;
   });
 }
 
 function getYoutubeChannelsMostPopularVideos() {
   const url = `https://www.googleapis.com/youtube/v3/search?key=${process.env.YOUTUBE_API_KEY}&channelId=${process.env.YOUTUBE_CHANNEL_ID}&part=snippet,id&maxResults=3&order=viewCount`;
   return sendGetRequest({ url }).then(({ items }) => {
-    const videoIds = items.map(({ id: { videoId } }) => videoId);
-    return videoIds;
+    const videos = items.map(({ id: { videoId }, snippet: { title } }) => ({
+      title,
+      videoId,
+    }));
+    return videos;
   });
 }
 
@@ -31,20 +41,22 @@ function addErrMessageToVideos(videos) {
   });
 }
 
-function addYoutubeVideoToList({ list, videoId, index }) {
+function addYoutubeVideoToList({ list, video: { videoId, title }, index }) {
   const videoTemplate = document.querySelector('#video__ytplayer');
   const video = videoTemplate.content
     .cloneNode(true)
     .querySelector('.video__ytplayer');
 
-  video.src = `https://www.youtube.com/embed/${videoId}`;
+  video.src = getVideoElSrc(videoId);
+  video.srcdoc = getVideoElSrcDoc({ videoId, title });
+  video.title = title;
   list[index].append(video);
 }
 
 getYoutubeChannelsNewestVideos()
-  .then((videoIds) => {
-    videoIds.forEach((videoId, i) => {
-      addYoutubeVideoToList({ list: videosNewVideos, videoId, index: i });
+  .then((videos) => {
+    videos.forEach((video, i) => {
+      addYoutubeVideoToList({ list: videosNewVideos, video, index: i });
     });
   })
   .catch((err) => {
@@ -54,9 +66,9 @@ getYoutubeChannelsNewestVideos()
   });
 
 getYoutubeChannelsMostPopularVideos()
-  .then((videoIds) => {
-    videoIds.forEach((videoId, i) => {
-      addYoutubeVideoToList({ list: videosPopularVideos, videoId, index: i });
+  .then((videos) => {
+    videos.forEach((video, i) => {
+      addYoutubeVideoToList({ list: videosPopularVideos, video, index: i });
     });
   })
   .catch((err) => {
