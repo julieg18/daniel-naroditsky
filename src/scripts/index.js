d2e41bbaa120e18e29d143e35f48f1c190f0988d
julieg18 <juliannagal18@gmail.com>
2021-02-09 08:06:37 -0600

    refactor: lazyload images and widgets

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,3 +3,6 @@ import './blocks/nav';
 import './blocks/videos';
 import '../pages/all.css';
 import '../pages/index.css';
+import 'lazysizes';
+import 'lazysizes/plugins/native-loading/ls.native-loading';
+import 'lazysizes/plugins/unveilhooks/ls.unveilhooks';
--- src/scripts/merch.js
+++ src/scripts/merch.js
@@ -2,3 +2,5 @@ import './blocks/head';
 import './blocks/nav';
 import '../pages/all.css';
 import '../pages/merch.css';
+import 'lazysizes';
+import 'lazysizes/plugins/native-loading/ls.native-loading';
