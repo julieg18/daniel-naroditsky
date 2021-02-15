05efd61ee3ba59e0aef22eab996ec83fc20fd963
julieg18 <juliannagal18@gmail.com>
2021-02-15 15:25:11 -0600

    feat: close nav menu if it loses focus

--- src/scripts/blocks/nav.js
+++ src/scripts/blocks/nav.js
@@ -10,10 +10,18 @@ function handlePageClick(e) {
   const isElMenuBtn =
     el.classList.contains('la-bars') ||
     el.classList.contains('nav__btn_icon_menu');
-  const isElNavItems = el === navItems || navItems.contains(el);
-  const isElNavLink = el.classList.contains('nav__link');
+  if (!el.closest('.nav__items') && !isElMenuBtn) {
+    // eslint-disable-next-line no-use-before-define
+    closeNavMenu();
+  }
+}
 
-  if ((!isElMenuBtn && !isElNavItems) || isElNavLink) {
+function handlePageKeyup() {
+  const el = document.activeElement;
+  const isElMenuBtn =
+    el.classList.contains('la-bars') ||
+    el.classList.contains('nav__btn_icon_menu');
+  if (!el.closest('.nav__items') && !isElMenuBtn) {
     // eslint-disable-next-line no-use-before-define
     closeNavMenu();
   }
@@ -22,11 +30,13 @@ function handlePageClick(e) {
 function openNavMenu() {
   navItems.classList.add('nav__items_show');
   pageBackground.addEventListener('click', handlePageClick);
+  pageBackground.addEventListener('keyup', handlePageKeyup);
 }
 
 function closeNavMenu() {
   navItems.classList.remove('nav__items_show');
   pageBackground.removeEventListener('click', handlePageClick);
+  pageBackground.removeEventListener('keyup', handlePageKeyup);
 }
 
 navMenuButton.addEventListener('click', openNavMenu);
